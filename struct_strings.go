33c2c19ef042b6735f20ef8f22575bb1301eac78
wd <wd@suboat.net>
2016-01-27 16:38:44 +0800

    add new method: a struct to map[string]string

--- struct_strings.go
+++ struct_strings.go
@@ -3,6 +3,7 @@ package gsf
 import (
 	"reflect"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -29,6 +30,54 @@ func StructToStringArray(src interface{}) (result []string, err error) {
 			switch item := tv.Value.Interface().(type) {
 			case time.Time:
 				result[i] = item.Format(time.RFC3339)
+			}
+		}
+	}
+	return
+}
+
+type structTagValue struct {
+	Tag   string
+	Value reflect.Value
+}
+
+func StructToStringMap(src interface{}, tags ...string) (result map[string]string, err error) {
+	var tag string
+	if len(tags) == 0 {
+		tag = "json"
+	} else {
+		tag = tags[0]
+	}
+	srcV, err := valueOf(src)
+	if err != nil {
+		return
+	}
+	srcTVs := getNameAndValues(srcV)
+	tvs := make([]structTagValue, len(srcTVs))
+	for _, sTV := range srcTVs {
+		t := sTV.Type.Tag.Get(tag)
+		if len(t) == 0 || strings.EqualFold(t, "-") {
+			continue
+		}
+		tvs = append(tvs, structTagValue{Tag: t, Value: sTV.Value})
+	}
+	result = make(map[string]string, len(tvs))
+	for _, tv := range tvs {
+		switch tv.Value.Kind() {
+		case reflect.Bool:
+			result[tv.Tag] = strconv.FormatBool(tv.Value.Bool())
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			result[tv.Tag] = strconv.FormatInt(tv.Value.Int(), 10)
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			result[tv.Tag] = strconv.FormatUint(tv.Value.Uint(), 10)
+		case reflect.Float32, reflect.Float64:
+			result[tv.Tag] = strconv.FormatFloat(tv.Value.Float(), 'f', 20, 64)
+		case reflect.String:
+			result[tv.Tag] = tv.Value.String()
+		case reflect.Struct:
+			switch item := tv.Value.Interface().(type) {
+			case time.Time:
+				result[tv.Tag] = item.Format(time.RFC3339)
 			}
 		}
 	}
--- test/struct_strings_test.go
+++ test/struct_strings_test.go
@@ -15,3 +15,13 @@ func TestStructToStringArray(t *testing.T) {
 	}
 	logStruct(t, "After", rs)
 }
+
+func TestStructToStringMap(t *testing.T) {
+	x := XXX{F0: "F000", F1: "F111", F2: 3, F3: time.Now(), F5: time.Now()}
+	logStruct(t, "Before", x)
+	rs, err := StructToStringMap(&x)
+	if err != nil {
+		t.Error(err)
+	}
+	logStruct(t, "After", rs)
+}
